abcef59d7902a8a491826a289fcdea3b7058dcb0
philhk <letsphil12@gmail.com>
2023-02-28 23:20:44 +0100

    improvements

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,16 @@ import { Plugin } from './bd';
 import React from 'react';
 
 module.exports = class TemplatePlugin extends Plugin {
+  private bdApi;
+
+  constructor() {
+    super();
+    this.bdApi = new BdApi('BetterScreensharePlugin');
+  }
+
   protected start(): void {
-    BdApi.UI.alert('Hello World!', <div>Hello World!</div>);
-    BdApi.UI.alert(
+    this.bdApi.UI.alert('Hello World!', <div>Hello World!</div>);
+    this.bdApi.UI.alert(
       'Hello World!',
       React.createElement('div', {}, 'Hello World')
     );
